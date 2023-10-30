e7d7d784f9c518e5b81ca230a2349bfada205c0b
Daniel_San <electronic.san@gmail.com>
2023-10-30 12:18:32 +0100

    add file kvetiny-spatne-frekvence.txt

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,19 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args)  {
+        System.out.println("\u001B[33mLekcia 5 - DOMACA ULOHA: Pokojove rastliny  - zadanie si vies precitat v  \u001B[0m" + "\u001B[36mReadmy.txt\u001B[0m");
+
         Plant plant1 = null,plant2 = null,plant3=null,plant4=null,plant5=null;
 
         try {
-      plant1 = new Plant("Růže", "Krásná červená růže", LocalDate.of(2023, 5, 15),
-              LocalDate.of(2023, 5, 20), 3);
+        plant1 = new Plant("Růže", "Krásná červená růže", LocalDate.of(2023, 5, 15),
+              LocalDate.of(2023, 5, 20), 0);
         plant2 = new Plant("Tulipán", "", LocalDate.of(2023, 5, 15),2);
         plant3 = new Plant("Fikus","Bonsai");
         plant5 = new Plant("Kaktus");
         } catch (PlantException e) {
             System.err.println("Chyba vytvaraní kvetiny: "+e.getLocalizedMessage());
         }
-            
-
-
-
-
-
 
 
             // Výpis informací o zálivce
@@ -35,7 +31,7 @@ public class Main {
         PlantList plants = new PlantList();
         plants.addPlant(plant2);
         plants.addPlant(plant3);
-        //vrati plantu zo zoznamu na 1.poyzicii
+        //vrati plantu zo zoznamu na 1.pozicii
         System.out.println(plants.getPlant(0));;
 
         plants.addPlant(plant5);
@@ -43,20 +39,18 @@ public class Main {
 
         // vypis zoznamu plants
         plants.printPlants();
+
         // vymazanie kvetiny
         plants.removePlantByName("Tulipán");
 
         //nacitanie kvetin zo suboru : "Kvetiny.txt"
         plants = makeLoadFromFile("Kvetiny.txt");
 
-
+        // vypisanie kvetin
         plants.printPlantsWithWateringInfo();
-            //ulozenie zoznamu do suboru :"NewKvetiny.txt"
-        try {
-            PlantList.saveToFile("NewKvetiny.txt", plants);
-        } catch (PlantException e) {
-            System.err.println("Chyba pri zapise do souboru : "+e.getLocalizedMessage());
-        }
+
+        //ulozenie zoznamu do suboru :"NewKvetiny.txt"
+        makeSavePlantsToFile(plants,"NewKvetiny.txt");
 
         // print info o zalevce kvetin
         plants.printPlantsWithWateringInfo();
@@ -70,20 +64,20 @@ public class Main {
         plants.printPlants();
 
             //zorad podla nazvu plant/kvetin
-        System.out.println("Zoradene podla nazvu:");
+        System.out.println("\u001B[32mZoradenie podla nazvu:\u001B[0m");
         Collections.sort(plants.getPlants());
         plants.printPlants();
 
         // Seřazení rostlin podle datumu poslední zálivky
-        System.out.println("Zoradene podla datumu zalievky :");
+        System.out.println("\u001B[32mZoradene podla datumu zalievky :\u001B[0m");
         Collections.sort(plants.getPlants(), new WateringDateComparator());
         plants.printPlantsWithWateringInfo();
 
         //DOPORUČENÉ OVĚŘENÍ NAVÍC  Poškozený soubor 1  : "kvetiny-spatne-datum.txt")
-        plants = makeLoadFromFile("kvetiny-spatne-datum.txt");
+    //    plants = makeLoadFromFile("kvetiny-spatne-datum.txt");
 
-
-
+        //DOPORUČENÉ OVĚŘENÍ NAVÍC  Poškozený soubor 1  : "kvetiny-spatne-frekvence.txt")
+    //    plants = makeLoadFromFile("kvetiny-spatne-frekvence.txt");
 
 
     }
@@ -98,7 +92,7 @@ public class Main {
     }
 
     private static PlantList  makeLoadFromFile(String filename) {   //Opatovne nacitanie suboru
-        System.out.println("Citanie zo suboru: ");
+        System.out.println("\u001B[34mCitanie zo suboru: \u001B[0m" + filename);
         PlantList plants = null;
         try {
             plants = PlantList.loadPlantsFromFile(filename);
---
+++ src/com/Engeto/Plant/Settings.java
@@ -0,0 +1,11 @@
+package com.Engeto.Plant;
+
+public class Settings {
+    private static final String fileItemDelimiterValue = "\t";
+
+
+    public static String fileItemDelimiter() {
+        return fileItemDelimiterValue;
+    }
+
+}
